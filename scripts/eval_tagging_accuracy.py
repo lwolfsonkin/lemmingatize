fixed accuracy calculation

scripts/eval_tagging_accuracy.py
@@ -1,4 +1,5 @@
 from operator import itemgetter
+import re
 import sys
 from typing import NamedTuple
 
@@ -15,7 +16,7 @@ class POSsed(NamedTuple):
 def output_generator(f_name, idx_col, form_col, lem_col, pos_col, mtag_col):
     with open(f_name, 'r') as to_f:
         indices_getter = itemgetter(idx_col, form_col, lem_col, pos_col, mtag_col)
-        for line in filter(lambda line: line[0] != '#' and line.strip(), to_f):
+        for line in filter(lambda line: not line.startswith('#') and line.strip(), to_f):
             row = line.strip().split('\t')
             try:
                 idx, word, lemma, pos, mtag = indices_getter(row)
@@ -24,15 +25,17 @@ def output_generator(f_name, idx_col, form_col, lem_col, pos_col, mtag_col):
                 print(row, file=sys.stderr)
                 raise e
 
+EMPTY_ENTRY_ = re.compile(r'^_(?:;_)*$')
+
 def tagging_accuracy(oracle_fname, pred_fname, train_fname, tag_to_str, idx_col, form_col, lem_col, pos_col, mtag_col, only_oov=False):
     conllu_open = lambda f_name: output_generator(f_name, idx_col, form_col, lem_col, pos_col, mtag_col)
 
     oracle = np.array([tag_to_str(p) for p in conllu_open(oracle_fname)])
     pred = np.array([tag_to_str(p) for p in conllu_open(pred_fname)])
-    criterion = ~np.char.startswith(oracle, '_')
+    not_empty_masker = np.vectorize(lambda p: not EMPTY_ENTRY_.match(p))
+    criterion = not_empty_masker(oracle)
     if only_oov:
         vocab = {p.word for p in conllu_open(train_fname)}
         isOOV = np.array([p.word not in vocab for p in conllu_open(oracle_fname)])
         criterion = criterion & isOOV
     return (pred == oracle)[criterion].mean()
-