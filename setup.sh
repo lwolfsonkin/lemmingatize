56fa84136e6fab03bd9ef7896712b153f84757f5
Lawrence <lawrencews@jhu.edu>
2018-12-21 12:24:03 -0500

    fix relative paths in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 ant -buildfile lib/cistern/marmot/build.xml
 python3 -m pip install -r requirements.txt
